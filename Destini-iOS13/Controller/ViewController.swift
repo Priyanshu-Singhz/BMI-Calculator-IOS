f1920507c6e79c4c8e90e66d82419efd33823ff5
“Nidhi <ps4151722@gmail.com>
2024-03-04 11:09:46 +0530

    added init

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let story0 : String = "You see a fork in the road."
+    let choice1 : String = "Take a left."
+
+    let choice2 : String = "Take a right."
+
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
